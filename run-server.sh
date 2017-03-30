0f6d387ddd85c68a81eb98dc77215d4661e5efdd
Adam King <rak@us.ibm.com>
2017-03-29 23:48:17 -0400

    export
    Signed-off-by: Adam King <rak@us.ibm.com>

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CONVERSATION_PASSWORD=$(echo "$BINDING" | jq -r '.password')
-CONVERSATION_USERNAME=$(echo "$BINDING" | jq -r '.username')
+export CONVERSATION_PASSWORD=$(echo "$BINDING" | jq -r '.password')
+export CONVERSATION_USERNAME=$(echo "$BINDING" | jq -r '.username')
 echo "CONVERSATION_USERNAME == $CONVERSATION_USERNAME"
 echo "CONVERSATION_PASSWORD == $CONVERSATION_PASSWORD"
 npm start
