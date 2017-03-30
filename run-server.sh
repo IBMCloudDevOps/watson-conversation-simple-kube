c70cbdc276396bc1c0ce27c2ddd782061c479c14
Adam King <rak@us.ibm.com>
2017-03-30 14:03:26 -0400

    Update env var names for CF consistency
    Signed-off-by: Adam King <rak@us.ibm.com>

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export CONVERSATION_PASSWORD=$(echo "$BINDING" | jq -r '.password')
-export CONVERSATION_USERNAME=$(echo "$BINDING" | jq -r '.username')
+export CONVERSATION_PASSWORD=$(echo "$CONVERSATION_SERVICE_BINDING" | jq -r '.password')
+export CONVERSATION_USERNAME=$(echo "$CONVERSATION_SERVICE_BINDING" | jq -r '.username')
 echo "CONVERSATION_USERNAME == $CONVERSATION_USERNAME"
 echo "CONVERSATION_PASSWORD == $CONVERSATION_PASSWORD"
 npm start
